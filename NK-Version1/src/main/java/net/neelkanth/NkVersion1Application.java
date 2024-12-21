03a3c9909fb19c1561278c29d33d075553aed002
unknown <singhrajpiyush@gmail.com>
2024-12-21 16:48:08 +0530

    First Commit in main Branch By Piyush

---
+++ NK-Version1/src/main/java/net/neelkanth/NkVersion1Application.java
@@ -0,0 +1,13 @@
+package net.neelkanth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkVersion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NkVersion1Application.class, args);
+	}
+
+}
---
+++ NK-Version1/src/test/java/net/neelkanth/NkVersion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.neelkanth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkVersion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
